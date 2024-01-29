56fff59dc3e1f86fb8e55fc8ac39a1e2cbaec705
Tharindu37 <tharindut520@gmail.com>
2024-01-29 21:20:56 +0530

    image capture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'webcam_with_angular';
+
+  stream: any = null;
+  status: any = null;
+  trigger: Subject<void> = new Subject();
+  previewImage: string = '';
+
+  get $trigger(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  snapshot(event: WebcamImage) {
+    console.log(event);
+    this.previewImage = event.imageAsDataUrl;
+  }
+
+  checkPermissions() {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: 500,
+          height: 500,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        this.stream = res;
+        this.status = 'My camera is accessing';
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err?.meesage == 'Permission denied') {
+          this.status =
+            'Permission denied please try again by approving the access';
+        } else {
+          this.status = 'You may not having camera system, Please try again';
+        }
+      });
+  }
+
+  captureImage() {
+    this.trigger.next();
+  }
 }
